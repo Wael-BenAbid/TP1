6dd175513135ecebca0a3890632260b6e5350c77
Wael-BenAbid <waelbenabid1@gmail.com>
2024-10-09 12:00:07 +0100

    first commit

---
+++ src/main/java/com/example/test/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.test.controller;
+
+import com.example.test.Entity.User;
+import com.example.test.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+}
---
+++ src/main/java/com/example/test/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.test.service;
+
+
+import com.example.test.Entity.User;
+import com.example.test.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+}
